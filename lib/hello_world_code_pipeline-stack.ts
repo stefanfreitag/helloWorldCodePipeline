245c4a9b7950b7e29378be4f69c13e7fdddc3dbf
Stefan Freitag <stefan.freitag@udo.edu>
2019-03-07 20:49:27 +0100

    Setup initial code pipeline

--- lib/hello_world_code_pipeline-stack.ts
+++ lib/hello_world_code_pipeline-stack.ts
@@ -1,9 +1,39 @@
 import cdk = require('@aws-cdk/cdk');
 
+import codecommit = require('@aws-cdk/aws-codecommit');
+import codebuild = require('@aws-cdk/aws-codebuild');
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+
 export class HelloWorldCodePipelineStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const repo = new codecommit.Repository(this, 'Repository', {
+      repositoryName: 'HelloWorldRepository',
+      description: 'A simple Hello World application.'
+    });
+
+    const buildProject = new codebuild.PipelineProject(this, 'Build', {
+      description: 'Build project for the HelloWorld application',
+      environment:
+          {buildImage: codebuild.LinuxBuildImage.UBUNTU_14_04_OPEN_JDK_9}
+    });
+
+    const sourceAction = new codecommit.PipelineSourceAction(
+        {branch: 'master', repository: repo, actionName: 'Source'});
+
+    const buildAction = new codebuild.PipelineBuildAction({
+      inputArtifact: sourceAction.outputArtifact,
+      actionName: 'Build',
+      project: buildProject
+    });
+
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'HelloWorldPipeline',
+      stages: [
+        {actions: [sourceAction], name: 'Source'},
+        {actions: [buildAction], name: 'Build'}
+      ]
+    });
   }
 }
