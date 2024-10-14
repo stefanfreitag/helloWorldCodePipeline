4a79813685eb12341fafe2d983292f80f1441f3c
Stefan Freitag <stefan.freitag@udo.edu>
2024-10-14 19:35:25 +0200

    chore: upgrade of cdk version

--- bin/hello_world_code_pipeline.ts
+++ bin/hello_world_code_pipeline.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { HelloWorldCodePipelineStack } from '../lib/hello_world_code_pipeline-stack';
-import { App } from '@aws-cdk/core';
+import { App } from 'aws-cdk-lib';
 
 const app = new App();
 new HelloWorldCodePipelineStack(app, 'HelloWorldCodePipelineStack');
